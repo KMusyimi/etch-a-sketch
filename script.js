5b468876a39e5e8f2c567ebf4229f2ea3adfc242
kenn mich <kennmusyimi97@gmail.com>
2023-09-06 11:59:07 +0300

    removed the test file

--- script.js
+++ script.js
@@ -1,14 +1,139 @@
 window.addEventListener("load", function ()
 {
+    let color = "rgba(112,112,112, 0.5)";
+    let mode = "";
     const gridWrapper = document.querySelector(".grid__wrapper");
-    gridWrapper.style.gridTemplateColumns = "repeat(16, 1fr)";
-    gridWrapper.style.gridTemplateRows = "repeat(16, 1fr)";
-    // const gridMedium = document.querySelector(".grid__medium");
-    for (i = 0; i < 256; i++)
+    // grid 48*48 square div
+    const smallGridCount = 36 * 36;
+    // grid 16*16 square div
+    const mediumGridCount = 16 * 16;
+    // grid 8*8 square div
+    const largeGridCount = 8 * 8;
+
+    populateGrid(mediumGridCount);
+    // add class name on click
+    
+    const gridSizeBtn = document.querySelectorAll("#grid__size > .btn");
+    for (i = 0; i < gridSizeBtn.length; i++)
     {
-        let square = document.createElement("div");
-        // square.style.backgroundColor = "blue";
-        square.className = "grid";
-        gridWrapper.insertAdjacentElement("beforeend", square)
+        gridSizeBtn[i].addEventListener("click", handleGridSizeEvt);
+    }
+
+    const colorMode = document.querySelectorAll("#colors__mode > .btn");
+    for (i = 0; i < colorMode.length; i++)
+    {
+        colorMode[i].addEventListener("click", handleColorBtnEvt);
+    }
+    
+    const eraser = document.querySelector(".eraser");
+    eraser.addEventListener("click", handleEraserEvt);
+
+    function populateGrid(count, size = "medium")
+    {
+
+        for (i = 0; i < count; i++)
+        {
+            let grid = document.createElement("div");
+            grid.className = `grid grid__${size}`;
+            grid.addEventListener("mouseover", handleColorModeEvt);
+            gridWrapper.insertAdjacentElement("beforeend", grid);
+
+        }
+    }
+
+    function resetGrid()
+    {
+        const grids = document.querySelectorAll(".grid");
+        for (let i = 0; i < grids.length; i++)
+        {
+            grids[i].parentNode.removeChild(grids[i]);
+
+        }
+    }
+
+
+    function getGridSize(evt)
+    {
+        const previousSelected = document.querySelector("#grid__size > .selected");
+        const clickedBtn = evt.target;
+        previousSelected.classList.remove("selected");
+
+        setTimeout(() =>
+        {
+            clickedBtn.classList.add("selected");
+        }, 30);
+        return clickedBtn;
+    }
+    function getColorMode(evt)
+    {
+        const previousSelected = document.querySelector("#colors__mode > .selected");
+        const clickedBtn = evt.target;
+        previousSelected.classList.remove("selected");
+
+        setTimeout(() =>
+        {
+            clickedBtn.classList.add("selected");
+        }, 30);
+        return clickedBtn;
+    }
+
+    function handleGridSizeEvt(evt)
+    {
+        const choice = getGridSize(evt);
+        const size = choice.getAttribute("id");
+        resetGrid();
+        switch (size)
+        {
+            case "small":
+                populateGrid(smallGridCount, "small");
+                break;
+            case "medium":
+                populateGrid(mediumGridCount, "medium");
+                break;
+            case "large":
+                populateGrid(largeGridCount, "large");
+                break;
+        }
+    }
+   
+    function handleColorBtnEvt(evt)
+    {
+        const choice = getColorMode(evt);
+        const colorMode = choice.getAttribute("id");
+        switch (colorMode)
+        {
+            case "classic":
+                mode = "classic";
+                color = "hsla(0, 0%, 43.9%, 0.5)";
+                break;
+            case "modern":
+                mode = modern;
+                color = "hsla(0, 0%, 43.9%, 1)";
+                break;
+            case "rainbow":
+                mode = colorMode;
+                break;
+
+        }
+
+    }
+    function calcColor(max, min, val)
+    {
+        const minHue = 240, maxHue = 0;
+        const currentPercent = (val - min) / (max - min);
+        return `hsla(${parseInt(Math.random(currentPercent*(maxHue-minHue)+minHue)*360)},100%, 50%, 1)`;
+        
+    }
+    function handleColorModeEvt(evt)
+    {
+        if (mode === "rainbow"){
+            color = calcColor(0, 9, 5);
+        }
+        evt.target.style.backgroundColor = color;
+    }
+    function handleEraserEvt(){
+        color = "hsla(204, 8%, 76%, 1)";
     }
 });
+
+
